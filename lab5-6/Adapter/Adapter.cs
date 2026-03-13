b276c054b6881eddb4c3460c111a9cb86b96d704
Oleksii Pereverzev <alekseypereverzevxl@gmail.com>
2026-03-13 15:18:28 +0200

    lab5-6

---
+++ lab5-6/Adapter/Adapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AdapterPatternTask
+{
+    public class MessageSender
+    {
+        public virtual void Send(string message, string recipient)
+        {
+            Console.WriteLine($"[Електронна пошта] Відправлено на {recipient}: {message}");
+        }
+    }
+
+    public class SMSsender
+    {
+        public void SendSMS(string message, string phoneNumber)
+        {
+            Console.WriteLine($"[SMS Сповіщення] Відправлено на номер {phoneNumber}: {message}");
+        }
+    }
+
+    public class TelegramSender
+    {
+        public void SendTelegram(string message, string username)
+        {
+            Console.WriteLine($"[Telegram Сповіщення] Відправлено користувачу {username}: {message}");
+        }
+    }
+
+    public class SmsAdapter : MessageSender
+    {
+        private SMSsender _smsSender = new SMSsender();
+
+        public override void Send(string message, string recipient)
+        {
+            _smsSender.SendSMS(message, recipient);
+        }
+    }
+
+    public class TelegramAdapter : MessageSender
+    {
+        private TelegramSender _telegramSender = new TelegramSender();
+
+        public override void Send(string message, string recipient)
+        {
+            _telegramSender.SendTelegram(message, recipient);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Робота системи сповіщень (Шаблон Adapter) ===\n");
+
+            MessageSender emailSender = new MessageSender();
+            emailSender.Send("Заявка на пiдключення прийнята.", "client@domain.com");
+
+            MessageSender smsSender = new SmsAdapter();
+            smsSender.Send("Аварiя на комутаторi агрегацiї усунена.", "+380501234567");
+
+            MessageSender telegramSender = new TelegramAdapter();
+            telegramSender.Send("Оновлення прошивки MikroTik завершено успiшно.", "@client_username");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab5-6/Proxy/Proxy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProxyPatternTask
+{
+    public interface IWebServer //Subject
+    {
+        string GetPage(string url);
+    }
+
+    public class RealWebServer : IWebServer // RealSubject
+    {
+        public string GetPage(string url)
+        {
+            Console.WriteLine($"[Мережа] Встановлення з'єднання та завантаження сторінки {url}...");
+
+            Thread.Sleep(2000);
+
+            return $"<html>Вміст веб-сторінки {url}</html>";
+        }
+    }
+
+    public class ProxyWebServer : IWebServer // Proxy
+    {
+        private RealWebServer _realServer;
+
+        private Dictionary<string, string> _pageCache = new Dictionary<string, string>(); // Cache
+
+        public string GetPage(string url)
+        {
+            if (_pageCache.ContainsKey(url))
+            {
+                Console.WriteLine($"[Кеш Проксі] Сторінку {url} знайдено в локальному кеші. Миттєва віддача.");
+                return _pageCache[url];
+            }
+
+            if (_realServer == null)
+            {
+                _realServer = new RealWebServer();
+            }
+
+            Console.WriteLine($"[Проксі] Сторінки {url} немає в кеші. Перенаправлення запиту до реального сервера...");
+
+            string pageContent = _realServer.GetPage(url);
+
+            _pageCache[url] = pageContent;
+
+            return pageContent;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Емулятор кешуючого проксі-сервера ===\n");
+
+            IWebServer proxy = new ProxyWebServer();
+
+            Console.WriteLine("--- Клієнт запитує www.knu.ua ---");
+            Console.WriteLine($"Результат: {proxy.GetPage("www.knu.ua")}\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine("--- Клієнт знову запитує www.knu.ua ---");
+            Console.WriteLine($"Результат: {proxy.GetPage("www.knu.ua")}\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine("--- Клієнт запитує www.google.com ---");
+            Console.WriteLine($"Результат: {proxy.GetPage("www.google.com")}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
